1a8394baae794456dda4e4fd6f2185adfb1a010f
Davorin <ddoung@sfu.ca>
2018-02-24 19:14:48 -0800

    Add MRV and degree heuristic checks

--- colour-graph.py
+++ colour-graph.py
@@ -1,20 +1,57 @@
 import pprint
 
+def tiebreaker(vertices, mrv_list):
+    if len(mrv_list) == 1:
+        return mrv_list[0]
+    else:
+        unassigned_var_total = []
+        
+        for vertex in vertices:
+            unassigned_var_total.append(len(vertex[1]))
+
+        max_constraints = max(unassigned_var_total)
+        for i in range(len(unassigned_var_total)):
+            if unassigned_var_total[i] == max_constraints:
+                return i
+
+
+def mrv(vertices): #return variable with least amount of colour options
+    free_colours_total = []
+    for vertex in vertices:
+        free_colours_total.append(len(vertex[2]))
+    print(free_colours_total)
+    mrv = min(free_colours_total)
+    print("mrv is: " + str(mrv))
+    mrv_list = []
+    for i in range(len(free_colours_total)):
+        if free_colours_total[i] == mrv:
+            mrv_list.append(i)
+    print("Vertices with MRV:")
+    print(mrv_list)
+
+    chosen_vertex = tiebreaker(vertices, mrv_list)
+    print("Chosen vertex: " + str(chosen_vertex))
+    return chosen_vertex
+
 def solve(n, k, G):
     free_colours = range(0, k)
-    vertices = {}
+    # vertices = {}
+    vertices = []
     for i in range(len(G)):
         unassigned_var = G[i][1:]
-        vertices[G[i][0]]=[None, unassigned_var, free_colours]
+        # vertices[G[i][0]]=[None, unassigned_var, free_colours]
+        vertices.append([None, unassigned_var, free_colours])
     # unsolveable =empty list
 	# return solution # solution is a list of pairs (each vertex and a colour) 
+    value = mrv(vertices)
     return vertices
 
 n = 3
 k = 3
-G = [[1,2,3],[2,1,3],[3,1,2]]
+G = [[1,2,3],[2,1,3,4],[3,1,2,4],[4,2,3]]
 solution = solve(n, k, G)
-print(solution)
+# for i in solution:
+#     print(i)
 
 def backtrack():
     return None
@@ -24,8 +61,4 @@ def constraint(vertices):
 
 def selectvertex():
     return None
-
-def mrv(vertices): #return variable with least amount of colour options
-    
-
 
